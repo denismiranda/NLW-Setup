e6e60b63bee37829943f0967dd9562874c82bff0
denismiranda <denismiranda148@gmail.com>
2023-01-18 21:58:17 -0400

    Editing

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const form = document.querySelector('#form-habits')
 let nlwSetup = new NLWSetup(form)
 
 const data = {
-    run: ["01-01", "01-02", "01-10",],
-    market: ["01-03", "01-02", "01-10"],
-    food: ["01-01", "01-02", "01-10"]
+    run: ["01-18", "01-20",],
+    water:["01-18","01-19","01-20"],
+    food: ["01-18", "01-19", "01-20"],
+    journal:["01-20"],
+    market: ["01-18",],
 }
 
 nlwSetup.setData(data)
